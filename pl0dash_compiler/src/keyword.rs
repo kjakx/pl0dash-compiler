61c06c6f12fa7b05b3d574d9738c2ec7cf4df786
kjakx <k.jak.xg0g11@gmail.com>
2022-06-19 13:34:44 +0000

    added missing trait

--- pl0dash_compiler/src/keyword.rs
+++ pl0dash_compiler/src/keyword.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::str::FromStr;
+use std::convert::TryFrom;
 
 #[derive(Debug, Clone)]
 pub struct UndefinedKeywordError;
--- pl0dash_compiler/src/symbol.rs
+++ pl0dash_compiler/src/symbol.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::convert::TryFrom;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Symbol {
